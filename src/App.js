f437ebb3e5d7899d646cbb5fcc5b2207551d70ac
akas33 <146909958+akas33@users.noreply.github.com>
2026-02-05 21:49:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,66 +1,125 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./App.css";
+import Success from "./components/Success";
 
-export default function ValentinePage() {
-  const [noCount, setNoCount] = useState(0);
-  const [yesPressed, setYesPressed] = useState(false);
-  const yesButtonSize = noCount * 20 + 16;
+// GIF/Photo Imports
+import flowerBear from "./flowerBear.gif";
+import cryingBear from "./crying.gif";
+import beggingBear from "./begging.gif";
+import madBear from "./madBear.gif";
+import heartBear from "./heart.gif"; 
+import pointBear from "./point.gif"; 
+import ourPhoto from "./our-photo.jpg"; 
 
-  const handleNoClick = () => {
+const App = () => {
+  const [opened, setOpened] = useState(false);
+  const [accepted, setAccepted] = useState(false);
+  const [noCount, setNoCount] = useState(0); 
+  const [yesButtonSize, setYesButtonSize] = useState(1);
+  const [noButtonPos, setNoButtonPos] = useState({ x: 0, y: 0 });
+  const iframeRef = useRef(null);
+
+  const handleOpenMessage = () => {
+    setOpened(true);
+    if (iframeRef.current) {
+      // Triggers the YouTube API to play and unmute
+      iframeRef.current.contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
+      iframeRef.current.contentWindow.postMessage('{"event":"command","func":"unMute","args":""}', '*');
+    }
+  };
+
+  const handleAccept = () => {
+    setAccepted(true);
+    if (window.confetti) {
+      window.confetti({ 
+        particleCount: 150, 
+        spread: 70, 
+        origin: { y: 0.6 },
+        colors: ['#ff4d6d', '#ffffff', '#ffccd5']
+      });
+    }
+  };
+
+  const handleReject = () => {
     setNoCount(noCount + 1);
+    setYesButtonSize(prev => prev + 0.35); 
+    
+    const maxWidth = window.innerWidth > 500 ? 140 : 70; 
+    const randomX = Math.floor(Math.random() * (maxWidth * 2)) - maxWidth;
+    const randomY = Math.floor(Math.random() * 80) - 40; 
+    setNoButtonPos({ x: randomX, y: randomY });
   };
 
-  const getNoButtonText = () => {
-    const phrases = [
-      "No", "Are you sure, Anant?", "Really sure?", "Think again!",
-      "Last chance!", "Surely not?", "You might regret this!",
-      "Give it another thought!", "Are you absolutely sure?",
-      "This could be a mistake!", "Have a heart!", "Don't be so cold!",
-      "Change of heart?", "Wouldn't you reconsider?", "Is that your final answer?",
-      "You're breaking my heart ;(",
-    ];
-    return phrases[Math.min(noCount, phrases.length - 1)];
-  };
+  const rejectionGifs = [flowerBear, cryingBear, beggingBear, madBear, heartBear, pointBear];
+  
+  const rejectionTexts = [
+    "No", 
+    "Are you sure, Anant? 🥺", 
+    "But we've been since Sep 2023! 😲", 
+    "I'm telling your mom! 🏃‍♂️", 
+    "Don't do this to me 💔", 
+    "Click the green one! 🫠✨"
+  ];
 
   return (
-    <div className="valentine-container">
-      {yesPressed ? (
-        <>
-          <img src="https://media.tenor.com/gUv1Ou87D8MAAAAi/milk-and-mocha.gif" alt="bear-kissing" />
-          <div className="text-container">Ok Yay!!!</div>
-          {/* Updated Song: Aankhon Se Batana - Autoplay starts at 24s */}
-          <iframe
-            width="0"
-            height="0"
-            src="https://www.youtube.com/embed/ZJ34geQLtUE?autoplay=1&start=24"
-            frameBorder="0"
-            allow="autoplay"
-          ></iframe>
-        </>
-      ) : (
-        <>
-          <img
-            className="h-[200px]"
-            style={{ width: "400px", height: "240px" }}
-            src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5pwz46268f.gif"
-            alt="bear-with-roses"
-          />
-          <h1 className="text-container">Will you be my Valentine, Anant?</h1>
-          <p className="established-tag">Established 25 Sep 2023</p>
-          <div>
-            <button
-              className={"yes-button"}
-              style={{ fontSize: yesButtonSize }}
-              onClick={() => setYesPressed(true)}
-            >
-              Yes
-            </button>
-            <button onClick={handleNoClick} className="no-button">
-              {noCount === 0 ? "No" : getNoButtonText()}
-            </button>
+    <div className="App">
+      {/* Updated Song: Aankhon Se Batana - Autoplay starts at 24s */}
+      <iframe 
+        ref={iframeRef} 
+        width="0" height="0" 
+        src="https://www.youtube.com/embed/ZJ34geQLtUE?enablejsapi=1&autoplay=1&mute=1&start=24&loop=1&playlist=ZJ34geQLtUE" 
+        allow="autoplay" 
+        style={{ display: 'none', position: 'absolute' }}>
+      </iframe>
+
+      <div className="App-body">
+        {!opened ? (
+          <div className="pulse">
+            <div className="bebe-tag">Established 25 Sep 2023</div>
+            <img src={ourPhoto} alt="Us" className="App-photo" />
+            <h1 className="App-text">I've been keeping a secret since 2023...</h1>
+            <button className="App-button btn-yes" onClick={handleOpenMessage}>Open ❤️</button>
           </div>
-        </>
-      )}
+        ) : !accepted ? (
+          <div className="asking-container">
+            <h1 className="App-text">Will you be my Valentine, Anant?</h1>
+            <img 
+              src={rejectionGifs[Math.min(noCount, rejectionGifs.length - 1)]} 
+              alt="Mood Bear" 
+              className="App-gif" 
+            />
+            
+            <div className="button-group">
+              <div className="yes-container">
+                <button 
+                  className="App-button btn-yes" 
+                  style={{ transform: `scale(${yesButtonSize})` }} 
+                  onClick={handleAccept}
+                >
+                  Yes
+                </button>
+              </div>
+
+              <button 
+                className="App-button btn-no" 
+                onClick={handleReject} 
+                onMouseEnter={handleReject}
+                style={{ 
+                    transform: `translate(${noButtonPos.x}px, ${noButtonPos.y}px)`,
+                    zIndex: 1000 
+                }}
+              >
+                {rejectionTexts[Math.min(noCount, rejectionTexts.length - 1)]}
+              </button>
+            </div>
+          </div>
+        ) : (
+          <Success />
+        )}
+      </div>
     </div>
   );
-}
+};
+
+export default App;
+                                         
